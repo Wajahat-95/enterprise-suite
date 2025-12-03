69b857100c505531d93764ea9df6faab8f04a3f5
Wajahat-95 <wajaht.siddiqui111@gmail.com>
2025-12-04 00:24:42 +0500

    feat: integrate TaskList component into secure Dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,7 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
+import TaskList from '@/components/TaskList.vue';
 </script>
 
 <template>
@@ -11,7 +12,7 @@ import { Head } from '@inertiajs/vue3';
             <h2
                 class="text-xl font-semibold leading-tight text-gray-800"
             >
-                Dashboard
+                Enterprise Task Dashboard
             </h2>
         </template>
 
@@ -21,7 +22,7 @@ import { Head } from '@inertiajs/vue3';
                     class="overflow-hidden bg-white shadow-sm sm:rounded-lg"
                 >
                     <div class="p-6 text-gray-900">
-                        You're logged in!
+                        <TaskList />
                     </div>
                 </div>
             </div>
